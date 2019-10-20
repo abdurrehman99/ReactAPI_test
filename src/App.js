3bdbdd65460412c374fe1e80b1cad282e71067de
Syed Abdur Rehman <syed.ebad1999@gmail.com>
2019-10-21 02:06:11 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import Login from './Login';
+import React, { Component } from 'react';
+import axois from 'axios';
+
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
   } from "react-router-dom";
-  
-  
 
-function App() {
-    return ( 
-        <Router>
-            <div className = "App" >
-            <button><a href='https://login.microsoftonline.com/common/adminconsent?client_id=e8a3c297-e162-48dc-9105-6a743c998cbe&state=testing&redirect_uri=http://localhost:3000/login'>click me</a></button>
+export default class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = { 
+            isVerified : '' , 
+            LeadId : '' , 
+            PinCode : '' ,
+            responseMessage : ''
+            }
+        }
+    
+    
+    componentDidMount(){
+        console.log("componentDidMount is called");
+        axois.get('https://salesfoo-config-api.azurewebsites.net/api/v1/IsConfigured/5F93931-BFD6-4FB7-92BE-882F6A94DFE7')
+        .then ( response => {
+            console.log(response.data.Data);
+            this.setState({ isVerified : response.data.Data })
+        })
+        .catch( error =>{
+            console.log(error);
+        })
+        
+    }
+
+    verifyPin = () => {
+        
+        const obj = {
+            LeadId : this.state.LeadId,
+            PinCode : this.state.PinCode
+
+        }
+    console.log(obj)
+        axois.post('https://salesfoo-config-api.azurewebsites.net/api/v1/VerifyPin/',obj)
+        .then ( response => {
+            console.log(response.data);
+            this.setState({ responseMessage : this.state.ResponseMessage });
+        })
+        .catch( error => {
+            console.log(error);
+        })
+    }
+
+    getPin = val => {
+        this.setState( { LeadId : '5F93931-BFD6-4FB7-92BE-882F6A94DFE7' , PinCode : val });
+        
+    }
+
+
+    render() {
+        return(
+            
+            <div>
+                {
+                    this.state.isVerified ? 
+                    <h2>You are verifed</h2> : 
+                    <div>
+                        <input type='text' onChange= { (event)=> { this.getPin(event.target.value) }} />
+                        <button onClick= { this.verifyPin }>Verify Pin</button>
+                        
+                    </div>
+                }
+                <h3>{this.state.responseMessage}</h3>
             </div>
-            <Route path='/login' component={Login} />
-        </Router>
-    );
+            
+        )
+            
+    }
 }
-
-export default App;
