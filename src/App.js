4d0cb1e6bcb66f0e519d5fe10da46c6987a71d21
Syed Abdur Rehman <syed.ebad1999@gmail.com>
2019-10-29 11:40:50 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import axois from 'axios';
-
+import Login from './Login';
+import queryString from 'query-string';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
   } from "react-router-dom";
+//   55F93931-BFD6-4FB7-92BE-882F6A94DFE7
 
 export default class App extends Component {
 
@@ -16,14 +18,16 @@ export default class App extends Component {
             isVerified : '' , 
             LeadId : '' , 
             PinCode : '' ,
-            responseMessage : ''
+            responseMessage : '',
+            login : true,
+            id : '',
             }
         }
     
     
     componentDidMount(){
         console.log("componentDidMount is called");
-        axois.get('https://salesfoo-config-api.azurewebsites.net/api/v1/IsConfigured/5F93931-BFD6-4FB7-92BE-882F6A94DFE7')
+        axois.get('https://salesfoo-config-api.azurewebsites.net/api/v1/IsConfigured/55f93931-bfd6-4fb7-92be-882f6a94dfe7')
         .then ( response => {
             console.log(response.data.Data);
             this.setState({ isVerified : response.data.Data })
@@ -41,15 +45,18 @@ export default class App extends Component {
             PinCode : this.state.PinCode
 
         }
-    console.log(obj)
         axois.post('https://salesfoo-config-api.azurewebsites.net/api/v1/VerifyPin/',obj)
         .then ( response => {
             console.log(response.data);
-            this.setState({ responseMessage : this.state.ResponseMessage });
+            this.setState({ 
+                responseMessage : response.data.ResponseMessage , 
+                login : response.data.Data 
+            });
         })
         .catch( error => {
             console.log(error);
-        })
+        });
+        
     }
 
     getPin = val => {
@@ -71,7 +78,14 @@ export default class App extends Component {
                         
                     </div>
                 }
-                <h3>{this.state.responseMessage}</h3>
+
+                <p>{this.state.responseMessage}</p>
+                {
+                    this.state.login ? 
+                    null : <button><a href='https://login.microsoftonline.com/common/adminconsent?client_id=e8a3c297-e162-48dc-9105-6a743c998cbe&state=testing&redirect_uri=http://localhost:3000'>Login</a></button>
+                }
+                <Login />
+                
             </div>
             
         )
--- src/Login.js
+++ src/Login.js
@@ -11,14 +11,14 @@ export default class Login extends Component {
     }
     componentDidMount()
         {
-            let params = queryString.parse(this.props.location.search);
+            let params = queryString.parse(this.props.location);
             console.log(params.tenant);
             this.setState({ id: params.tenant })
         }
     render() {
         return (
             <div>
-                <h2>You are logged in !</h2>
+                
                 <p>{this.state.id}</p>
             </div>
         )
