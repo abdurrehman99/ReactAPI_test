6d09cea24d89676e3dac29ecb982cfe746e57d96
Syed Abdur Rehman <syed.ebad1999@gmail.com>
2019-11-12 00:28:04 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import axois from 'axios';
+import { BrowserRouter as  Router, Switch,Route ,Link } from "react-router-dom";
 import Login from './Login';
 import queryString from 'query-string';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-//   55F93931-BFD6-4FB7-92BE-882F6A94DFE7
+
+//55F93931-BFD6-4FB7-92BE-882F6A94DFE7
+//A2BC39FC-63EB-4B61-91E7-FC516B6E5454
+//573485
 
 export default class App extends Component {
 
@@ -16,32 +14,50 @@ export default class App extends Component {
         super(props);
         this.state = { 
             isVerified : '' , 
-            LeadId : '' , 
+            LeadId : '1B2C086D-DBE2-4197-9467-93C232AF5921' , 
             PinCode : '' ,
             responseMessage : '',
-            login : true,
+            login : false,
             id : '',
             }
         }
-    
-    
     componentDidMount(){
-        console.log("componentDidMount is called");
-        axois.get('https://salesfoo-config-api.azurewebsites.net/api/v1/IsConfigured/55f93931-bfd6-4fb7-92be-882f6a94dfe7')
+
+        axois.get('https://salesfoo-config-api.azurewebsites.net/api/v1/IsConfigured/1B2C086D-DBE2-4197-9467-93C232AF5921')
         .then ( response => {
             console.log(response.data.Data);
             this.setState({ isVerified : response.data.Data })
         })
         .catch( error =>{
             console.log(error);
+        });
+
+        let params = new URLSearchParams(window.location.search);
+        let lead = params.get('tenant');
+
+        this.setState({ id : lead });
+
+        const config = {
+            LeadId : this.state.LeadId,
+            DomainUrl : 'http://localhost:3000',
+            TenantId : lead,
+            TanentName : 'Ebad',
+            DomainUserName : 'Syed',
+            UserAzureEmail : 'admin@salfoo.com'
+        }
+        console.log(config);
+        axois.post('https://salesfoo-config-api.azurewebsites.net/api/v1/SaveConfiguration/',config)
+        .then( response => {
+            console.log(response.data.ResponseMessage);
         })
-        
+        .catch( error => {
+            console.log(error.Message);
+        });
     }
 
     verifyPin = () => {
-        
         const obj = {
-            LeadId : this.state.LeadId,
+            LeadId : 'A2BC39FC-63EB-4B61-91E7-FC516B6E5454',
             PinCode : this.state.PinCode
 
         }
@@ -60,35 +76,37 @@ export default class App extends Component {
     }
 
     getPin = val => {
-        this.setState( { LeadId : '5F93931-BFD6-4FB7-92BE-882F6A94DFE7' , PinCode : val });
+        this.setState( { PinCode : val });
+    }
+    saveConfig = () => {
         
     }
-
-
     render() {
         return(
-            
-            <div>
+           <div>
+            <div> 
                 {
                     this.state.isVerified ? 
                     <h2>You are verifed</h2> : 
                     <div>
                         <input type='text' onChange= { (event)=> { this.getPin(event.target.value) }} />
-                        <button onClick= { this.verifyPin }>Verify Pin</button>
-                        
+                        <button onClick= { this.verifyPin }>Verify Pin</button>   
                     </div>
                 }
 
                 <p>{this.state.responseMessage}</p>
                 {
-                    this.state.login ? 
-                    null : <button><a href='https://login.microsoftonline.com/common/adminconsent?client_id=e8a3c297-e162-48dc-9105-6a743c998cbe&state=testing&redirect_uri=http://localhost:3000'>Login</a></button>
+                    this.state.login ? null 
+                    : <button><a href='https://login.microsoftonline.com/common/adminconsent?client_id=f0e68c6e-ef76-4e76-ab4d-0b14b71876bd&state=admin_consent&redirect_uri=http://localhost:3000'>Login</a></button> 
                 }
-                <Login />
-                
+                {
+                    this.state.id != null 
+                }
+
+            </div>
+            <h3>{this.state.id}</h3>
             </div>
             
         )
-            
     }
 }
--- src/Login.js
+++ src/Login.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import queryString from 'query-string';
+import axois from 'axios';
+import { BrowserRouter as  Router, Switch,Route ,Link } from "react-router-dom";
 
 export default class Login extends Component {
     
-    constructor(props){
-        super(props);
-        this.state = {
-            id : '',
-        }
-    }
+   
+
     componentDidMount()
         {
-            let params = queryString.parse(this.props.location);
-            console.log(params.tenant);
-            this.setState({ id: params.tenant })
+
+            
         }
+
+        
+
+        
+
     render() {
         return (
-            <div>
-                
-                <p>{this.state.id}</p>
-            </div>
+            <div></div>
         )
     }
 }
